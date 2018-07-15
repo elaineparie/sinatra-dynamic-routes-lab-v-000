b1e1cda7c5e36be9ea739e8abe9c8b9c81627672
Elaine Anderson <eparie@bu.edu>
2018-07-15 15:54:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -36,6 +36,9 @@ class App < Sinatra::Base
   get "/:operation/:number1/:number2" do
     @number1 = params[:number1]
     @number2 = params[:number2]
+    num1 = @number1.to_i
+    num2 = @number2.to_i
+    num1 + num2
   end
 
 end
