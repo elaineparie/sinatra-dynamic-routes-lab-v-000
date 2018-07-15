83c89393f06dbd3e89c348212fa288974dbf0ab7
Elaine Anderson <eparie@bu.edu>
2018-07-15 15:49:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,6 +31,6 @@ get "/say/:word1/:word2/:word3/:word4/:word5"
 @word4 = params[:word4]
 @word5 = params[:word5]
 
-"#{@word1}#{@word2}#{@word3}#{@word4}#{@word5}"
+#{}"#{@word1}#{@word2}#{@word3}#{@word4}#{@word5}"
 
 end
