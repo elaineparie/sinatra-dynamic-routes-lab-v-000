0e868ab80dcb6d9f2331f2ee6f6c16342a867f60
Elaine Anderson <eparie@bu.edu>
2018-07-15 16:14:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -39,6 +39,7 @@ class App < Sinatra::Base
     @number2 = params[:number2]
     if @operation.include?(add)
       @number1.to_i + @number2.to_i
+      binding.pry
     end
     if @operation.include?(divide)
     number = @number1.to_i / @number2.to_i
